04ebf0d8c0c3618ce1751ba06db6254e69ff0710
DevEricOfc <eric.d.sv2@gmail.com>
2024-02-25 17:48:06 -0300

    Adição tela de login e cadastro
    obs: corrigir erro h2

---
+++ StreetRushGear/src/main/java/com/example/streetrushgear/StreetRushGearApplication.java
@@ -0,0 +1,13 @@
+package com.example.streetrushgear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetRushGearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreetRushGearApplication.class, args);
+    }
+
+}
---
+++ StreetRushGear/src/test/java/com/example/streetrushgear/StreetRushGearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.streetrushgear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetRushGearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
